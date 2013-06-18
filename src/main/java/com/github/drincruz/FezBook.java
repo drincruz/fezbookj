a511ad9adbd1164284dc996318733712a52e872e
Adrian J. Cruz <drincruz@gmail.com>
2013-06-18 16:30:56 -0400

    code cleanup

--- src/main/java/com/github/drincruz/FezBook.java
+++ src/main/java/com/github/drincruz/FezBook.java
@@ -1,12 +1,14 @@
 /**
  * FezBook.java
- * @version 0.1.2
+ * @version 0.1.3
  * @author Adrian J. Cruz
  * @license http://www.opensource.org/licenses/bsd-license.php <2013>, <Adrian J. Cruz> 
  */
-package FezBook;
+package com.github.drincruz.fezbookj;
 
-import java.net.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.MalformedURLException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -17,20 +19,20 @@ public class FezBook {
     private static final String ME = "https://graph.facebook.com/me/";
     private String token;
 
-    /*
-     * constructor
+    /**
+     * Constructor
      * @param String atoken
      */
     public FezBook(String atoken) {
-        this.token = atoken;
+        token = atoken;
     }
 
-    /*
+    /**
      * Opens a URI
      * @param String uri
      * @return Bufferedreader b the URI content
      */
-    public BufferedReader openuri(String uri) throws MalformedURLException {
+    public BufferedReader openUri(String uri) throws MalformedURLException {
         BufferedReader b = null;
         try {
             URL u = new URL(uri);
@@ -47,16 +49,16 @@ public class FezBook {
         return b;
     }
 
-    /*
+    /**
      * Gets the Facebook accounts
      * @param long fbid Facebook ID
      * @return String line the JSON returned that contains the account information
      */
-    public String get_accounts(long fbid) {
-        String uri = this.FBGRAPH + fbid + "/accounts?access_token=" + this.token;
+    public String getAccounts(long fbid) {
+        String uri = FBGRAPH + fbid + "/accounts?access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -70,15 +72,15 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Gets the Facebook accounts
      * @return String line the JSON returned that contains the account information
      */
-    public String get_accounts() {
-        String uri = this.ME + "accounts?access_token=" + this.token;
+    public String getAccounts() {
+        String uri = ME + "accounts?access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -92,16 +94,16 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Get Facebook info
      * @param long fbid Facebook ID
      * @return String line the JSON returned that contains information for fbid
      */
-    public String get_fbdata(long fbid) {
-        String uri = this.FBGRAPH + fbid + "?access_token=" + this.token;
+    public String getFBData(long fbid) {
+        String uri = FBGRAPH + fbid + "?access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -115,15 +117,15 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Get Facebook info
      * @return String line the JSON returned that contains information for ME
      */
-    public String get_fbdata() {
-        String uri = this.ME + "?access_token=" + this.token;
+    public String getFBData() {
+        String uri = ME + "?access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -137,16 +139,16 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Get Facebook feed
      * @param long fbid Facebook ID
      * @return String line the JSON returned that contains the feed information
      */
-    public String get_feed(long fbid) {
-        String uri = this.FBGRAPH + fbid + "/feed&access_token=" + this.token;
+    public String getFeed(long fbid) {
+        String uri = FBGRAPH + fbid + "/feed&access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -160,15 +162,15 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Get Facebook feed
      * @return String line the JSON returned that contains the feed information
      */
-    public String get_feed() {
-        String uri = this.ME + "feed&access_token=" + this.token;
+    public String getFeed() {
+        String uri = ME + "feed&access_token=" + token;
         String line = null;
         try {
-            BufferedReader b = this.openuri(uri);
+            BufferedReader b = openUri(uri);
             try {
                 line = b.readLine();
             }
@@ -182,16 +184,16 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Post to Facebook feed
      * @param String postdata URLEncode'd data
      * @return String line the JSON returned that contains the post information
      */
-    public String post_to_feed(String postdata) {
-        String uri = this.ME + "feed?access_token=" + this.token;
+    public String postToFeed(String postdata) {
+        String uri = ME + "feed?access_token=" + token;
         String line = null;
         try {
-            /* Send data */
+            // Send data
             URL url = new URL(uri);
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
@@ -199,7 +201,7 @@ public class FezBook {
             wr.write(postdata);
             wr.flush();
 
-            /* Get the response */
+            // Get the response
             BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             line = rd.readLine();
             wr.close();
@@ -211,17 +213,17 @@ public class FezBook {
         return line;
     }
 
-    /*
+    /**
      * Post to Facebook feed
      * @param String postdata URLEncode'd data
      * @param long fbid Facebook ID
      * @return String line the JSON returned that contains the post information
      */
-    public String post_to_feed(String postdata, long fbid) {
-        String uri = this.FBGRAPH + fbid + "/feed?access_token=" + this.token;
+    public String postToFeed(String postdata, long fbid) {
+        String uri = FBGRAPH + fbid + "/feed?access_token=" + token;
         String line = null;
         try {
-            /* Send data */
+            // Send data
             URL url = new URL(uri);
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
@@ -229,7 +231,7 @@ public class FezBook {
             wr.write(postdata);
             wr.flush();
 
-            /* Get the response */
+            // Get the response
             BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             line = rd.readLine();
             wr.close();
